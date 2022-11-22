01400c37b1e42d8e77ac6aeb37f4a25de5e63816
KaluTheKova <kalle.puutio@gmail.com>
2022-11-22 15:01:24 +0200

    gateway getmessages test added

--- gateway/gateway.go
+++ gateway/gateway.go
@@ -4,6 +4,8 @@ import (
 	"io"
 	"log"
 	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 const (
@@ -11,30 +13,55 @@ const (
 	httpservAddress string = "http://localhost:8080/"
 )
 
+// album represents data about a record album.
+type album struct {
+	ID     string  `json:"id"`
+	Title  string  `json:"title"`
+	Artist string  `json:"artist"`
+	Price  float64 `json:"price"`
+}
+
+// albums slice to seed record album data.
+var albums = []album{
+	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
+	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
+	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+}
+
 // When requested, returns content of the file created by OBSE
 func main() {
 	log.Printf("API GATEWAY STARTING")
-	http.HandleFunc("/messages", getMessages)
-	//http.HandleFunc("/state", putState)
-	//http.HandleFunc("/state", getState)
-	//http.HandleFunc("/run-log", getRunLog)
-	http.ListenAndServe(":8083", nil)
+
+	router := gin.Default()
+	router.GET("/messages", getMessages)
+	router.PUT("/state", putState)
+	router.GET("/state", getState)
+	router.GET("/run-log", getRunLog)
+
+	router.Run("localhost:8083")
 }
 
 // GET /messages (as text/plain)
 // Returns all message registered with OBSE-service. Assumed implementation
 // forwards the request to HTTPSERV and returns the result.
-func getMessages(writer http.ResponseWriter, req *http.Request) {
-	respBody := GetMessagesAPICall(httpservAddress)
+func getMessages(c *gin.Context) {
+	log.Println("Received GET/messages") // DEBUG
+	resp, err := http.Get(httpservAddress)
+	if err != nil {
+		log.Println(err)
+	}
+	defer resp.Body.Close()
 
-	writer.WriteHeader(http.StatusOK)
-	writer.Header().Set("Content-Type", "application/text")
-	writer.Write([]byte(respBody))
-	return
+	log.Println("Reading response") // DEBUG
+	respBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Println(err)
+	}
+	c.String(http.StatusOK, string(respBody))
 }
 
-// GetMessagesAPICall sends http.Get to given address and returns bytecontent
-func GetMessagesAPICall(url string) []byte {
+// GetMessagesAPICall executes http.Get to given address and returns bytecontent
+/* func getMessagesFromHttpserv(url string) []byte {
 
 	log.Println("Received GET/messages") // DEBUG
 	resp, err := http.Get(url)
@@ -49,7 +76,7 @@ func GetMessagesAPICall(url string) []byte {
 		log.Println(err)
 	}
 	return respBody
-}
+} */
 
 // PUT /state (payload “INIT”, “PAUSED”, “RUNNING”, “SHUTDOWN”)
 // PAUSED = ORIG service is not sending messages
@@ -57,12 +84,20 @@ func GetMessagesAPICall(url string) []byte {
 // If the new state is equal to previous nothing happens.
 // There are two special cases:
 // INIT = everything (except log information for /run-log and /messages) is in the
-// initial state and ORIG starts sending again,
-//  state is set to RUNNING
+// initial state and ORIG starts sending again, state is set to RUNNING
 // SHUTDOWN = all containers are stopped
+func putState(c *gin.Context) {
+
+}
 
 // GET /state (as text/plain)
 // get the value of state
+func getState(c *gin.Context) {
+
+}
 
 // GET /run-log (as text/plain)
 // Get information about state changes
+func getRunLog(c *gin.Context) {
+
+}
--- gateway/gateway_test.go
+++ gateway/gateway_test.go
@@ -1,35 +1,28 @@
 package main
 
 import (
+	"io/ioutil"
 	"net/http"
+	"net/http/httptest"
 	"testing"
 
-	"github.com/steinfletcher/apitest"
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
 )
 
 // https://apitest.dev/
 
-func testGetMessagesAPICall(t *testing.T) {
-	var messagesArray [3]string
-	messagesArray[0] = "2022-11-11T18:01:38.011Z 1 MSG_1 to compse140.i"
-	messagesArray[1] = "2022-11-11T18:11:58.08Z 1 MSG_1 to compse140.i"
-	messagesArray[2] = "2022-11-11T18:12:39.646Z 1 MSG_1 to compse140.i"
+// Needs to have httpserv running
+func TestGetMessagesAPICall(t *testing.T) {
+	mockFilecontents := []byte("2022-11-22T11:01:32.149Z 1 MSG_{1} to compse140.o\n2022-11-22T11:01:35.155Z 2 Got MSG_{2} to compse140.i\n2022-11-22T11:01:38.156Z 3 Got MSG_{3} to compse140.i")
+	//mockFilecontents := []byte("2022-11-22T11:01:32.149Z 1 MSG_{1} to compse140.o" + "2022-11-22T11:01:35.155Z 2 Got MSG_{2} to compse140.i" + "2022-11-22T11:01:38.156Z 3 Got MSG_{3} to compse140.i")
 
-	messages := []byte("2022-11-11T18:01:38.011Z 1 MSG_1 to compse140.i\n2022-11-11T18:11:58.08Z 1 MSG_1 to compse140.i\n2022-11-11T18:12:39.646Z 1 MSG_1 to compse140.i")
+	router := gin.Default()
+	router.GET("/messages", getMessages)
+	req, _ := http.NewRequest("GET", "/messages", nil)
+	writer := httptest.NewRecorder()
+	router.ServeHTTP(writer, req)
 
-	handler := func(w http.ResponseWriter, r *http.Request) {
-		msg := `{"message": "hello"}`
-		_, _ = w.Write([]byte(msg))
-		w.WriteHeader(http.StatusOK)
-	}
-
-	// Test API gateway's response to calls to it
-	// Example: Get /messages returns mock messages
-	apitest.New(). // configuration
-			HandlerFunc(GetMessagesAPICall()).
-			Get("/message"). // request
-			Expect(t).       // expectations
-			Body(`{"message": "hello"}`).
-			Status(http.StatusOK).
-			End()
+	responseData, _ := ioutil.ReadAll(writer.Body)
+	assert.Equal(t, string(mockFilecontents), string(responseData), "Get test file contents from httpserv")
 }
